fe43d0296bf7004a1c09aa61f9ae0fd4b1ee9df4
hikida.satoshi <s.hikida@hotmail.co.jp>
2013-06-17 21:16:11 +0900

    fixed __unicode__() of arp_calendar.models.ArpHoliday

--- chrono/arp_calendar/models.py
+++ chrono/arp_calendar/models.py
@@ -6,11 +6,11 @@ class HolidayType(models.Model):
     name_ja = models.CharField(max_length=30)
 
     def __unicode__(self):
-        return self.name_en
+        return self.name_ja
 
 class ArpHoliday(models.Model):
     date = models.DateField(auto_now=False, auto_now_add=False, primary_key=True)
     holiday_type = models.ForeignKey('HolidayType')
 
     def __unicode__(self):
-        return self.date.strftime('%Y/%m/%d:') + self.type.name_en
+        return self.date.strftime('%Y/%m/%d:') + self.holiday_type.name_ja
